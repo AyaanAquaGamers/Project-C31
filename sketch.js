2fc317ad2603b6434ea9955b1b60f75658aff041
AyaanAquaGamers <72394681+AyaanAquaGamers@users.noreply.github.com>
2021-05-16 10:52:54 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,82 +1,82 @@
-const Engine = Matter.Engine
-const World =Matter.World
-const Bodies=Matter.Bodies
-var engine,world
-var drops=[]
-var maxDrops=100
-var umbrella
-var rand
-var night 
-var Thunder,thunder1,thunder2,thunder3,thunder4
-var thunderCreatedFrame=0
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Body = Matter.Body,
+  Bodies = Matter.Bodies;
+ 
+var particles = [];
+var plinkos = [];
+var divisions =[];
+var divisionHeight=300;
+var score =0;
+
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+  for (var i = 0; i <=width; i = i + 80) {
+    divisions.push(new Divisions(i, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+  //create 1st row of plinko objects
+  for (var j = 75; j <=width; j=j+50) { 
+    plinkos.push(new Plinko(j,75));
+  }
+
+  //create 2nd row of plinko objects
+  for (var j = 50; j <=width-10; j=j+50) 
+  {
+    plinkos.push(new Plinko(j,175));
+  }
+
+  //create 3rd row of plinko objects
+  for (var j = 75; j <=width; j=j+50) { 
+    plinkos.push(new Plinko(j,275));
+  }
+  
+  //create 4th row of plinko objects
+  for (var j = 50; j <=width-10; j=j+50) 
+  {
+    plinkos.push(new Plinko(j,375));
+  }
 
 
-function preload(){
+  //create particle objects
+
+  Engine.run(engine)
+  
     
-    thunder1=loadImage("1.png")
-    thunder2=loadImage("2.png")
-    thunder3=loadImage("3.png")
-    thunder4=loadImage("4.png")
 }
+ 
 
-function setup(){
+
+function draw() {
+ rectMode(CENTER)
+ background(0)
+
+//drawSprites()
+
+  
    
-    var canvas=createCanvas(500,700)
-
-    engine=Engine.create()
-    world=engine.world
-
-    umbrella=new Umbrella(200,500)
-
-    if (frameCount%80===0){
-    for(var i=0;i<maxDrops;i++){
-        drops.push(new createDrops(random(0,500),random(0,500)))
-    }
-}
-    
+  for (var n = 0; n<divisions.length; n++) {
+    divisions[n].display();   
+  }
+   
+if(frameCount%60===0){
+  particles.push(new Particle(random(width/2-30, width/2+30),10,10))
 }
 
-function draw(){
-    Engine.update(engine)
-    background("red")
-
-
-rand=Math.round(random(1,4))
-if(frameCount%80 === 0){
-    thunderCreatedFrame=frameCount
-    Thunder=createSprite(random(10,370),random(10,30),10,10)
-    switch(rand){
-        case 1:Thunder.addImage(thunder1)
-        break
-        case 2:Thunder.addImage(thunder2)
-        break
-        case 3:Thunder.addImage(thunder3)
-        break
-        case 4:Thunder.addImage(thunder4)
-        break
-        default:break
-    }
-    Thunder.scale=0.7
+for(var h=0; h<particles.length;h++){
+  particles[h].display()
 }
 
-
-    if(thunderCreatedFrame+100===frameCount&&Thunder){
-        Thunder.destroy()
-    }
-
-
-
-    umbrella.display()
-
-    for(var i=0; i < maxDrops; i++){
-        drops[i].display()
-        drops[i].update()
-    }
-    
-    
-
-
-    
-    drawSprites()
+for(var j=0; j<plinkos.length;j++){
+  plinkos[j].display()
 }
 
+ground.display()
+
+  //display the paricles 
+
+}
